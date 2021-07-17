f9e22df435c1b22a60382ca71b249f0bfec62235
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-07-17 13:13:53 -0300

    windows: mklinks: custom arch linux icon on WT

--- win/scripts/mklinks.bat
+++ win/scripts/mklinks.bat
@@ -16,10 +16,13 @@ for %%s in ("%SCRIPTS_PATH%\"*) do (
 set SCOOP_APPS_PATH=%USERPROFILE%\scoop\apps
 
 :: Windows terminal
-set WT_PATH=%LocalAppData%\Microsoft\Windows^ Terminal
+set WT_CONFIG_PATH=%LocalAppData%\Microsoft\Windows^ Terminal
 set WT_SETTINGS_FILE=settings.json
-del "%WT_PATH%\%WT_SETTINGS_FILE%"
-mklink "%WT_PATH%\%WT_SETTINGS_FILE%" "%WIN_ROOT%\wt\%WT_SETTINGS_FILE%"
+del "%WT_CONFIG_PATH%\%WT_SETTINGS_FILE%"
+mklink "%WT_CONFIG_PATH%\%WT_SETTINGS_FILE%" "%WIN_ROOT%\wt\%WT_SETTINGS_FILE%"
+set WT_PATH="%SCOOP_APPS_PATH%\windows-terminal\current"
+set ICON_PATH="ProfileIcons\arch_16px.ico"
+copy "%WIN_ROOT%\wt\%ICON_PATH%" "%WT_PATH%\%ICON_PATH%" /Y > NUL
 
 :: PulseAudio
 set PULSE_PATH="%SCOOP_APPS_PATH%\pulseaudio\current\bin"
